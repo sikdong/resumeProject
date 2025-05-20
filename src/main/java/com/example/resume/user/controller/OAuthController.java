669822cf5161bb7795bef521fa5d52219de1ccdf
sikdong <zmstnv1119@naver.com>
2025-05-20 22:30:00 +0900

    로그인 성공 시 리다이렉트 로직 수정

--- src/main/java/com/example/resume/user/controller/OAuthController.java
+++ src/main/java/com/example/resume/user/controller/OAuthController.java
@@ -2,37 +2,45 @@ package com.example.resume.user.controller;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.example.resume.common.jwt.JwtUtil;
+import com.example.resume.user.domain.User;
+import com.example.resume.user.dto.UserAdditionalInfoRequestDto;
+import com.example.resume.user.repository.UserRepository;
+import com.example.resume.user.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/oauth")
+@RequiredArgsConstructor
 public class OAuthController {
 
-    @Value("${jwt.secret}")
-    private String jwtSecret;
-    private static final String ADDITIONAL_FORM_URL = "http://43.203.221.191//app/wishy/page-682b2a605d83947b1f4c94f5";
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
 
     @GetMapping("/success")
     public void oauthSuccess(HttpServletResponse response,
                              @AuthenticationPrincipal OAuth2User principal) throws IOException {
-        String email = principal.getAttribute("email");
 
-        String token = JWT.create()
-                .withSubject(email)
-                .withIssuedAt(new Date())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // 1일
-                .sign(Algorithm.HMAC256(jwtSecret)); // 이 키는 강력하고 안전하게!
+        User user = userService.getUser(principal);
+        String redirectUrl = userService.getRedirectUrl(response, user);
 
-        String redirectUrl = ADDITIONAL_FORM_URL+"??token=" + token;
+        String token = jwtUtil.createToken(user.getEmail());
+
+        redirectUrl += "?token=" + token;
 
         response.sendRedirect(redirectUrl);
     }
