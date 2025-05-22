038e6dbef97ff24477ac6cf73775f5b06f1235c9
sikdong <zmstnv1119@naver.com>
2025-05-22 17:38:55 +0900

    이력서 조회 최신순으로 변경

--- src/main/java/com/example/resume/resume/repository/ResumeRepository.java
+++ src/main/java/com/example/resume/resume/repository/ResumeRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -15,6 +16,6 @@ public interface ResumeRepository extends JpaRepository<Resume, Long> {
     @Query("SELECT r FROM Resume r LEFT JOIN FETCH r.evaluations WHERE r.id = :resumeId")
     Optional<Resume> findByIdWithEvaluation(@Param("resumeId") Long resumeId);
 
-    @Query("SELECT r FROM Resume r LEFT JOIN FETCH r.evaluations")
+    @Query("SELECT r FROM Resume r LEFT JOIN FETCH r.evaluations ORDER BY r.createdAt desc")
     List<Resume> findAllWithEvaluation();
 }
