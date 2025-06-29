0a131fbd5088d9ebd8e9a735427ac21a89efc196
sikdong <zmstnv1119@naver.com>
2025-06-29 10:09:12 +0900

    평가 조회 시 평가자 이름 조회되도록 수정

--- src/main/java/com/example/resume/evaluation/dto/EvaluationResponseDto.java
+++ src/main/java/com/example/resume/evaluation/dto/EvaluationResponseDto.java
@@ -8,14 +8,16 @@ public record EvaluationResponseDto(
         Long id,
         Double score,
         LocalDateTime evaluatedAt,
-        String comment
+        String comment,
+        String memberName
 ) {
     public static EvaluationResponseDto fromEntity(Evaluation evaluation){
         return new EvaluationResponseDto(
                 evaluation.getId(),
                 evaluation.getScore(),
                 evaluation.getCreatedAt(),
-                evaluation.getComment()
+                evaluation.getComment(),
+                evaluation.getEvaluator().getName()
         );
     }
 }
