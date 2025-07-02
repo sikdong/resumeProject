7306bc7b243bf6e874555fd0e79b0a68e0a48f37
sikdong <zmstnv1119@naver.com>
2025-07-02 22:33:30 +0900

    이력서 entity ai 추출 키워드 null 처리

--- src/main/java/com/example/resume/resume/domain/Resume.java
+++ src/main/java/com/example/resume/resume/domain/Resume.java
@@ -3,19 +3,26 @@ import com.example.resume.evaluation.domain.Evaluation;
 import com.example.resume.user.domain.Member;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
 @Getter @Setter @Builder
 @NoArgsConstructor @AllArgsConstructor
+@DynamicInsert
 public class Resume {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String fileUrl;
+
+    @ColumnDefault("")
     private String keyword;
-    @Column(name = "view_count", nullable = false, columnDefinition = "bigint default 0")
+
+    @Column(name = "view_count", columnDefinition = "bigint default 0")
     private Long viewCount;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
