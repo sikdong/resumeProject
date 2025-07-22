de88d662c85743acbc1e02f9bb4aaf7b5115899e
sikdong <zmstnv1119@naver.com>
2025-07-22 15:31:24 +0900

    api 별 응답시간 test 위한 aop 어노테이션 추가

---
+++ src/main/java/com/example/resume/common/aop/ExecutionTimeAspect.java
@@ -0,0 +1,34 @@
+package com.example.resume.common.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Slf4j
+@Aspect
+@Component
+public class ExecutionTimeAspect {
+    @Around("@annotation(com.example.resume.common.annotation.LogExecutionTime)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        Object result = joinPoint.proceed();
+
+        stopWatch.stop();
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        String className = methodSignature.getDeclaringType().getSimpleName();
+        String methodName = methodSignature.getName();
+
+        log.info("[실행시간] {}.{}: {}ms",
+                className,
+                methodName,
+                stopWatch.getTotalTimeMillis());
+
+        return result;
+    }
+}
