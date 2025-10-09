23650b199af864ad8b9e39ae66951f8feb7fa900
sikdong <zmstnv1119@naver.com>
2025-10-09 21:43:21 +0900

    feat: 비회원일 때 principal 수정

--- src/main/java/com/example/resume/config/SecurityConfig.java
+++ src/main/java/com/example/resume/config/SecurityConfig.java
@@ -26,9 +26,12 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET, "/api/resumes/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/resume-search/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/token/**").permitAll()
                         .anyRequest().authenticated()
+                )
+                .anonymous(anonymous -> anonymous
+                        .principal("0")
+                        .authorities("ROLE_ANONYMOUS")
                 )
                 .headers(headers -> headers
                         .frameOptions(frameOptions -> frameOptions.disable())
