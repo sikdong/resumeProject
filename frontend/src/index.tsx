6c9f3d1adc0e172d6af45c36f4b7ba3d6e9b3fcd
sikdong <zmstnv1119@naver.com>
2025-10-20 21:13:02 +0900

    refactor: 데이터 fetch시 중복 쿼리 발생 오류로 인해 Strict Mode 제거

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -9,11 +9,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <ToastProvider>
-      <App />
-    </ToastProvider>
-  </React.StrictMode>
+  <ToastProvider>
+    <App />
+  </ToastProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
